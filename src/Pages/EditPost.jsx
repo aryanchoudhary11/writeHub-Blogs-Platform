82de2cf89ee21841b079e556280e85ae7976a400
Aryan <119430003+aryanchoudhary11@users.noreply.github.com>
2025-07-04 11:47:52 +0530

    EditPost authorization done

--- src/Pages/EditPost.jsx
+++ src/Pages/EditPost.jsx
@@ -2,11 +2,13 @@ import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import appwriteService from "../Appwrite/config";
 import { Container, PostForm } from "../components/index";
+import { useSelector } from "react-redux";
 
 function EditPost() {
   const [post, setPost] = useState(null);
   const { slug } = useParams();
   const navigate = useNavigate();
+
   useEffect(() => {
     if (slug) {
       appwriteService.getPost(slug).then((post) => {
@@ -18,12 +20,36 @@ function EditPost() {
       navigate("/");
     }
   }, [slug, navigate]);
-  return post ? (
+  const userData = useSelector((state) => state.auth.userData);
+  useEffect(() => {
+    if (post && userData) {
+      console.log("Post.userId:", post.userID);
+      console.log("UserData.$id:", userData.$id);
+    }
+  }, [post, userData]);
+
+  const isAuthor = post && userData && post.userID === userData.$id;
+  if (!post) return null;
+
+  if (!isAuthor) {
+    return (
+      <div className="py-8">
+        <Container>
+          <h1 className="text-2xl font-bold text-red-500">
+            Unauthorized Access
+          </h1>
+          <p className="text-gray-600">You cannot edit someone else's post.</p>
+        </Container>
+      </div>
+    );
+  }
+
+  return (
     <div className="py-8">
       <Container>
         <PostForm post={post} />
       </Container>
     </div>
-  ) : null;
+  );
 }
 export default EditPost;
